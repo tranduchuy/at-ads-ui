d924c16a7d408e3f6afbd3de8597bb6a3dd7154a
Hoang Nguyen <nhsh1997@gmail.com>
2019-07-16 08:07:02 +0700

    init module

--- src/app/main/add-adwords-accounts/add-adwords-accounts.module.ts
+++ src/app/main/add-adwords-accounts/add-adwords-accounts.module.ts
@@ -3,11 +3,16 @@ import { CommonModule } from '@angular/common';
 import { AddAdwordsAccountsComponent } from './add-adwords-accounts.component';
 import { AddAdwordsAccountsRoutingModule } from './add-adwords-accounts-routing.module';
 
+import { FuseSharedModule } from '@fuse/shared.module';
+
+import {MatGridListModule} from '@angular/material/grid-list';
 @NgModule({
   declarations: [AddAdwordsAccountsComponent],
   imports: [
     CommonModule,
-    AddAdwordsAccountsRoutingModule
+    AddAdwordsAccountsRoutingModule,
+    MatGridListModule,
+    FuseSharedModule
   ]
 })
 export class AddAdwordsAccountsModule { }
