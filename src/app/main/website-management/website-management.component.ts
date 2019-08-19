532d80c0384b405c4f6c76b76e253f2976a3f2c3
Tue Vo <vothaiminhtue0312@gmail.com>
2019-08-19 17:18:18 +0700

    BUILD PRODUCT 19/08/2019 5:18PMM

--- src/app/main/website-management/website-management.component.ts
+++ src/app/main/website-management/website-management.component.ts
@@ -175,8 +175,6 @@ export class WebsiteManagementComponent extends EditableFormBaseComponent implem
   generatePostObject() {
     const body = { ...this.form.value };
 
-    //body.domain = body.domain.replace(/\/$/,'');
-
     const params = {
       domain: body.domain,
       accountId: this.selectedAccountId
@@ -188,26 +186,23 @@ export class WebsiteManagementComponent extends EditableFormBaseComponent implem
   post() {
     this.isProcessing = true;
     const params = this.generatePostObject();
-
-    console.log(params);
-
     this.isProcessing = false;
 
-    // this._fuseProgressiveBarService.show();
-    // const sub = this._websiteManagementService.addWebsite(params)
-    //   .subscribe((res: ILoginSuccess) => {
-    //     this._dialogService._openSuccessDialog(res);
-    //     this._fuseProgressiveBarService.hide();
-    //     this.isProcessing = false;
-    //     this.getWebsites();
-    //   },
-    //     (error: HttpErrorResponse) => {
-    //       this._dialogService._openErrorDialog(error.error);
-    //       this._fuseProgressiveBarService.hide();
-    //       this.isProcessing = false;
-    //     }
-    //   );
-    // this.subscriptions.push(sub);
+    this._fuseProgressiveBarService.show();
+    const sub = this._websiteManagementService.addWebsite(params)
+      .subscribe((res: ILoginSuccess) => {
+        this._dialogService._openSuccessDialog(res);
+        this._fuseProgressiveBarService.hide();
+        this.isProcessing = false;
+        this.getWebsites();
+      },
+        (error: HttpErrorResponse) => {
+          this._dialogService._openErrorDialog(error.error);
+          this._fuseProgressiveBarService.hide();
+          this.isProcessing = false;
+        }
+      );
+    this.subscriptions.push(sub);
   }
 
   onSelectAdsId(event) {
