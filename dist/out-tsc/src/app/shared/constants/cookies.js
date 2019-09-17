944e6ef7760b383296b8f10e26b6b214e071cd33
HuyTD <vincenthuytd@gmail.com>
2019-09-17 07:23:18 +0700

    Build 17-09-2019

--- dist/out-tsc/src/app/shared/constants/cookies.js
+++
@@ -1,9 +0,0 @@
-const prefix = '__ata__';
-const CookieNames = {
-    token: prefix + '_token',
-    user: prefix + '_user',
-    activeAccountId: prefix + '_active_account_id',
-    activeAdsAccountId: prefix + '_active_ads_account_id'
-};
-export default CookieNames;
-//# sourceMappingURL=cookies.js.map
--- dist/out-tsc/src/app/shared/models/i-auth-reset-password.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=i-auth-reset-password.js.map
