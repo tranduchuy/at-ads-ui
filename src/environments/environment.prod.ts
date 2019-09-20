5bc933e55ce82af6089927e3e4d2af28f8c5973f
HuyTD <vincenthuytd@gmail.com>
2019-09-20 15:50:34 +0700

    Change config environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   hmr: false,
-  hostApi: 'https://api-click.appnet.edu.vn',
+  hostApi: 'https://api.click.apte.asia',
   googleAuth2ClientID: '246364548184-qi67l6sl8p9rlv8e8ev1p6329caf8gfr.apps.googleusercontent.com',
-  hostTracking: 'https://static-click.appnet.edu.vn/static/tracking.js',
-  oauth2RedirectUri: 'https://click.appnet.edu.vn'
+  hostTracking: 'https://static.click.apte.asia/static/tracking.js',
+  oauth2RedirectUri: 'https://click.apte.asia'
 };
