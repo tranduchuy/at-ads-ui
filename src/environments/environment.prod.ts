e5679acbbfa3320a6423e2f32b15a8174f9b06af
HuyTD <vincenthuytd@gmail.com>
2019-10-07 21:53:13 +0700

    Update config of environment production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   hmr: false,
-  hostApi: 'https://api.click.apte.asia',
+  hostApi: 'https://api.x2.com.vn',
   googleAuth2ClientID: '246364548184-qi67l6sl8p9rlv8e8ev1p6329caf8gfr.apps.googleusercontent.com',
-  hostTracking: 'https://static.click.apte.asia/static/tracking.js',
-  oauth2RedirectUri: 'https://click.apte.asia'
+  hostTracking: 'https://static.x2.com.vn/static/tracking.js',
+  oauth2RedirectUri: 'https://x2.com.vn'
 };
