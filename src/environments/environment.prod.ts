201330a709e44b58f7119be9a4ddd15140f98e5e
HuyTD <vincenthuytd@gmail.com>
2019-10-19 10:43:36 +0700

    Build 19-10-2019

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   hmr: false,
-  hostApi: 'https://api.click.apte.asia',
+  hostApi: 'https://api.x2.com.vn',
   googleAuth2ClientID: '246364548184-qi67l6sl8p9rlv8e8ev1p6329caf8gfr.apps.googleusercontent.com',
   hostTracking: 'https://static.x2.com.vn/static/tracking.js',
   oauth2RedirectUri: 'https://x2.com.vn',
